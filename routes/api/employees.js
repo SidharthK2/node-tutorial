716afc677f17c306720574b5df16f7d408bbf412
sidharthK2 <sidharthkapoor.100@gmail.com>
2022-10-12 13:41:13 +0530

    add JWT

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const { request } = require("https");
 const router = express.Router();
 const employeesController = require("../../controllers/employeesController");
+const verifyJWT = require("../../middleware/verifyJWT");
 
 router
   .route("/")
-  .get(employeesController.getAllEmployees)
+  .get(verifyJWT, employeesController.getAllEmployees)
   .post(employeesController.createNewEmployees)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee);
